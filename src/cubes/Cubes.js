477c4cb9e4ac268ae597174952f667265325aa91
Justin Kim <justinkim2015@gmail.com>
2023-02-25 22:51:40 +0900

    Initial commit

---
+++ src/cubes/Cubes.js
@@ -0,0 +1,16 @@
+import Cube from './cube/Cube'
+
+function Cubes() {
+  return(
+    <div>
+      <Cube></Cube>
+      <Cube></Cube>
+      <Cube></Cube>
+      <Cube></Cube>
+      <Cube></Cube>
+      <Cube></Cube>
+    </div>
+  )
+}
+
+export default Cubes
---
+++ src/cubes/cube/Cube.js
@@ -0,0 +1,11 @@
+import './cube.css'
+
+function Cube() {
+  return(
+    <div class='cube'>
+      
+    </div>
+  )
+}
+
+export default Cube
